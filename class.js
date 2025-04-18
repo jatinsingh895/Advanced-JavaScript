96c711dda30e2cb979a28a37925814f5b11b1c60
jatinsingh895 <jatinsingh5572@gmail.com>
2025-04-18 13:00:15 +0530

    Class

--- class.js
+++ class.js
@@ -55,21 +55,140 @@
 
 //METHODS
 //Functions on a class are called methods.When defining these methods,we dont use function keyword.we start directly with name
-class Person {
-    constructor(firstname,lastname){
-        this.firstname=firstname;
-        this.lastname=lastname;
-    }
-    greet(){
-        console.log("hi there! I'm",this.firstname);
-    }
-    compliment(name,object){
-        return "That's a wonderful" + object+ ","+name;
+// class Person {
+//     constructor(firstname,lastname){
+//         this.firstname=firstname;
+//         this.lastname=lastname;
+//     }
+//     greet(){
+//         console.log("hi there! I'm",this.firstname);
+//     }
+//     compliment(name,object){
+//         return "That's a wonderful" + object+ ","+name;
 
-    }
-}
-let p= new Person("kashish","singh");
-p.greet();
-let compliment=p.compliment("Harry","hat");
-//apne hi attributes bna kar krenge
-console.log(compliment);
+//     }
+// }
+// let p= new Person("Jatin","singh");
+// p.greet();
+// let compliment=p.compliment("Harry","hat");
+// //apne hi attributes bna kar krenge
+// console.log(compliment);
+// PROPERTIES-> they atre sometimes called the field hold the data of the class
+// class Person
+// {
+//     constructor(firstname,lastname){
+//         this.firstname=firstname;
+//         this.lastname=lastname;
+//     }
+// }
+// let p=new Person("malik","van putten");
+// console.log("hi",p.firstname);
+// private
+// class Person{
+//     #firstname
+//     #lastname
+//     constructor(firstname,lastname){
+//                 this.firstname=firstname;
+//                 this.lastname=lastname;
+//             }
+//         }
+//         let p=new Person("malik","van putten");
+//         console.log("hi",p.firstname);
+// class Person{
+//     #firstname;
+//     #lastname;
+//     constructor(firstname,lastname)
+//     {
+//         if(firstname.startsWith("M")){
+//             this.#firstname=firstname;
+//         }else
+//         {
+//             this.#firstname="M"+firstname;
+//         }
+//         this.#lastname=lastname
+//     }
+
+// }
+// let p=new Person("kay","waxon");
+// console.log(p.firstname);
+// class Person {
+//     #firstname;
+//     #lastname;
+
+//     constructor(firstname, lastname) {
+//         this.#firstname = firstname;
+//         this.#lastname = lastname;
+//     }
+
+//     get firstname() {
+//         return this.#firstname;
+//     }
+
+//     set firstname(firstname) {
+//         this.#firstname = firstname;
+//     }
+
+//     get lastname() {
+//         return this.#lastname;
+//     }
+
+//     set lastname(lastname) {
+//         this.#lastname = lastname;
+//     }
+// }
+// let p=new Person("JATIN","SINGH");
+// console.log(p.firstname);
+// INHERITANCE
+// class Vehicle {
+//     constructor(color, currentSpeed, maxSpeed) {
+//         this.color = color;
+//         this.currentSpeed = currentSpeed;
+//         this.maxSpeed = maxSpeed;
+//     }
+
+//     move() {
+//         console.log("moving at", this.currentSpeed);
+//     }
+
+//     accelerate(amount) {
+//         this.currentSpeed +=amount;
+//     }
+// }
+
+// class Motorcycle extends Vehicle {
+//     constructor(color, currentSpeed, maxSpeed, fuel) {
+//         super(color, currentSpeed, maxSpeed);  
+//         this.fuel = fuel;
+//     }
+//     doWheelie(){
+//         console.log("Driving on one wheel!");
+//     }
+// }
+// let myBike = new Motorcycle("black", 60, 200, "pet");
+
+// console.log("Color:", myBike.color);
+// console.log("Current Speed:", myBike.currentSpeed);
+// console.log("Max Speed:", myBike.maxSpeed);
+// console.log("Fuel:", myBike.fuel);
+
+// myBike.move();
+// myBike.accelerate(40);
+// myBike.move();
+// myBike.doWheelie();
+//PROTOTYPES->A prototype is the mechanism in JavaScript that makes it possible to have objects.When nothing is specified when creating a class, the objects inherit from the object.prototype prototype.
+// class Person {
+//     constructor(firstname, lastname) {
+//         this.firstname = firstname;
+//         this.lastname = lastname;
+//     }
+//     greet() {
+//         console.log("Hi there! I'm", this.firstname);
+//     }
+//     introduce() {
+//         console.log("Hi, I'm", this.firstname);
+//     }
+// }
+// Person.prototype.favoriteColor = "green";
+// let p = new Person("jatin", "singh");
+// console.log(p.favoriteColor);
+// p.introduce();
