f570787bffde818bbf04e518ce37eeee07f1b8d8
jatinsingh895 <jatinsingh5572@gmail.com>
2025-04-23 11:30:59 +0530

    Maps

---
+++ maps.js
@@ -0,0 +1,48 @@
+const fruits = new Map([
+    ['apples', 500],
+    ['bananas', 200],
+    ['oranges', 300]
+]);
+
+// // const fruits = new Map();
+// // fruits.set('apples', 500);
+// // fruits.set('bananas', 200);
+// fruits.set('oranges', 700);
+// console.log(fruits);
+// console.log(fruits.get('apples'));
+// console.log(typeof fruits);
+// console.log(fruits instanceof Map);
+
+
+//JAVASCRIPT MAP METHODS
+
+// fruits.delete("apples");
+// console.log(fruits.size);
+// fruits.clear();
+// console.log(fruits.size);
+// console.log(fruits.has("apples"));
+
+// let text="";
+// fruits.forEach(function(value,key){
+//     text+=key+'='+value;
+// })
+// console.log(text);
+// console.log(typeof(text));
+
+// let text="";
+// for(const x of fruits.entries()){
+//     text+=x;
+// }
+// console.log(text);
+// let text="";
+// for(const x of fruits.keys())
+// {
+//     text+=x;
+// }
+// console.log(text);
+// let text="";
+// for(const x of fruits.values())
+// {
+//     text+=x;
+// }
+// console.log(text);
